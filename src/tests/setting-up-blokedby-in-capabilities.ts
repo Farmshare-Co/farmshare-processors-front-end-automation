0e2d3826927ebaff419694b9333491538075f825
Gawo <kevgawo@gmail.com>
2025-02-04 19:11:09 +0300

    new changes for tests Co-authored-by: Taylor <liquidg3@users.noreply.github.com>

---
+++ src/tests/setting-up-blokedby-in-capabilities.ts
@@ -0,0 +1,26 @@
+// import { assert } from '@sprucelabs/test-utils'
+import Runner from '../Runner/Runner'
+import wait from '../Runner/wait'
+
+void (async () => {
+    const runner = await Runner.Runner()
+    await runner.login()
+    await runner.waitForSelector('[data-rr-ui-event-key="capabilities"]')
+    await runner.click('[data-rr-ui-event-key="capabilities"]')
+
+    await runner.waitForSelector('button')
+    await wait(3000)
+
+    //select chuck roast
+    await runner.clickAtIndex('button', 15);
+
+    //make it blocked by arm roast
+    await runner.select('select', '66fac7b8e56b9f6d628a5401');
+    await runner.clickDoneInDialog()
+
+    //confirm it's blocked the other way
+    //select arm roast
+    await runner.clickAtIndex('button', 19);
+
+    // await runner.shutdown()
+})()
